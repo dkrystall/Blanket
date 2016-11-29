47704d9029da646cb1ec196e2132a667495cbb07
David Krystall <david.krystall@ontometrics.com>
2016-11-28 18:41:19 -0800

    initial commit for Kotlin Blanket project

---
+++ src/Blanket.kt
@@ -0,0 +1,12 @@
+/**
+ * Blanket
+ * Created by davidkrystall on 11/28/16.
+ *
+ *
+ * (c) ontometrics 2015, All Rights Reserved
+ */
+class Blanket {
+    fun sayHello() : String {
+        return "Helo"
+    }
+}
---
+++ src/Main.kt
@@ -0,0 +1,15 @@
+/**
+ * Blanket
+ * Created by davidkrystall on 11/28/16.
+ *
+ *
+ * (c) ontometrics 2015, All Rights Reserved
+ */
+import Blanket
+
+class Main {
+    fun main() {
+        var thingy = Blanket()
+        println("Thingy output:"+thingy.sayHello())
+    }
+}
